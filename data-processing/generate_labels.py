d8b41c8f1dd58a8bcb1c9caf2c592cc026c7d9bb
Susmita Khadka <susmitakhadka81@gmail.com>
2025-11-28 17:12:39 +0545

    update generate_labels code and labels.csv file

--- data-processing/generate_labels.py
+++ data-processing/generate_labels.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-dataset_dir = "nsl_interpreter\processed"
+dataset_dir = "landmark_sequences"
 output_csv = "labels.csv"
 
 rows = []
@@ -10,13 +10,16 @@ for class_name in os.listdir(dataset_dir):
     class_path = os.path.join(dataset_dir, class_name)
     if not os.path.isdir(class_path):
         continue
-    for video_folder in os.listdir(class_path):
-        video_path = os.path.join(class_path, video_folder)
-        rows.append([video_path, class_name])
+
+    # loop through files
+    for file_name in os.listdir(class_path):
+        if file_name.endswith(".npy"):          # only select landmark files
+            full_path = os.path.join(class_path, file_name)
+            rows.append([full_path.replace("\\", "/"), class_name])
 
 with open(output_csv, "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerow(["video_path", "label"])
+    writer.writerow(["file_path", "label"])
     writer.writerows(rows)
 
 print(f"✅ Labels saved in {output_csv}. Total sequences: {len(rows)}")
